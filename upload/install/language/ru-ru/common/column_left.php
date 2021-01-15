4e309d44350a91c03e46835ae708220058bfdf86
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-01-15 22:14:16 +0300

    Актуализация

--- upload/install/language/ru-ru/common/column_left.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Text
-$_['text_license']       = 'Лицензия';
-$_['text_installation']  = 'Предварительная установка';
-$_['text_configuration'] = 'Конфигурация';
-$_['text_upgrade']       = 'Обновить';
-$_['text_finished']      = 'Завершить';
-$_['text_language']      = 'Язык';
-$_['text_name']          = 'Русский (Российская Федерация)';
--- upload/install/language/ru-ru/common/footer.php
+++ upload/install/language/ru-ru/common/footer.php
@@ -3,4 +3,4 @@
 $_['text_project']       = 'Домашняя страница проекта';
 $_['text_documentation'] = 'Документация';
 $_['text_support']       = 'Форумы поддержки</a>&nbsp;&nbsp;|&nbsp;&nbsp; <a href="https://docs.opencart.name/" target="_blank"><b>Перевод документации</b></a>&nbsp;&nbsp;|&nbsp;&nbsp; <a href="https://forum.opencart.name/" target="_blank"><b>Русскоязычный форум</b>';
-$_['text_footer']        = 'Авторские права © 2014 OpenCart - Все права защищены';
+$_['text_footer']        = '<a href="https://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' Все права защищены.<br><a href="https://forum.opencart.name">Перевод OpenCart</a> &copy; 2020-' . date('Y') . ' Все права защищены.';
